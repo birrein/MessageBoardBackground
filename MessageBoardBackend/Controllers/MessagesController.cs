918315f16c6bf8ca88db3d9483591785c6c423fa
Manuel Marín <birrein@gmail.com>
2017-12-08 00:18:04 -0300

    Hasta Capítulo 3

---
+++ MessageBoardBackend/Controllers/MessagesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MessageBoardBackend.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        public IEnumerable<Models.Message> Get()
+        {
+            return new Models.Message[] {
+                new Models.Message {
+                    Owner = "John",
+                    Text  = "hello"
+                },
+                new Models.Message {
+                    Owner = "Manuel",
+                    Text  = "wena compare!"
+                }
+            };
+        }
+    }
+}
