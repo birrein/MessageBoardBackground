96c22f9062ebf139ce3822ba6baf592d060a02cd
Manuel Marín <birrein@gmail.com>
2017-12-18 01:16:37 -0300

    Hasta 9.2

---
+++ MessageBoardBackend/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MessageBoardBackend.Controllers
+{
+    public class JwtPacket
+    {
+        public string Token
+        {
+            get;
+            set;
+        }
+    }
+
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost("register")]
+        public JwtPacket Register([FromBody]Models.User user)
+        {
+            var jwt = new JwtSecurityToken();
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return new JwtPacket() { Token = encodedJwt };
+        }
+    }
+}
